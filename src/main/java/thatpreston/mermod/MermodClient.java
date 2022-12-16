3187a0be06c230c3a1dcee9343bd63893ea0a2a6
ThatPreston <prestoncoopergames@gmail.com>
2022-12-16 17:06:28 -0600

    Use events instead of a mixin to add the tail layer

--- src/main/java/thatpreston/mermod/MermodClient.java
+++ src/main/java/thatpreston/mermod/MermodClient.java
@@ -1,17 +1,39 @@
 package thatpreston.mermod;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeableLeatherItem;
-import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import thatpreston.mermod.client.render.TailModel;
+import thatpreston.mermod.client.render.TailRenderLayer;
 
+@Mod.EventBusSubscriber(modid = "mermod", value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MermodClient {
     public static final ModelLayerLocation TAIL = new ModelLayerLocation(new ResourceLocation("mermod", "tail"), "tail");
-    public static void init() {
+    @SubscribeEvent
+    public static void clientInit(final FMLClientSetupEvent event) {
         Minecraft.getInstance().getItemColors().register((stack, index) -> index > 0 ? ((DyeableLeatherItem)stack.getItem()).getColor(stack) : -1, RegistryHandler.SEA_NECKLACE.get());
         Minecraft.getInstance().getItemColors().register((stack, index) -> ((DyeableLeatherItem)stack.getItem()).getColor(stack), RegistryHandler.MERMAID_BRA_MODIFIER.get(), RegistryHandler.TAIL_GRADIENT_MODIFIER.get());
-        ForgeHooksClient.registerLayerDefinition(TAIL, () -> TailModel.getTexturedModelData());
+    }
+    @SubscribeEvent
+    public static void registerLayer(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(TAIL, TailModel::getTexturedModelData);
+    }
+    @SubscribeEvent
+    public static void addLayer(final EntityRenderersEvent.AddLayers event) {
+        for(String skin : event.getSkins()) {
+            LivingEntityRenderer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderer = event.getSkin(skin);
+            if(renderer != null) {
+                renderer.addLayer(new TailRenderLayer(renderer, event.getEntityModels()));
+            }
+        }
     }
 }
